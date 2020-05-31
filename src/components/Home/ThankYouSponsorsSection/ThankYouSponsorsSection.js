8c5c2ffd2c4b504a144e3eef0278a6a27bf12e21
Elaine Gao <gao.elaine164@gmail.com>
2020-05-31 15:31:20 +1000

    wrap components in containers

--- src/components/Home/ThankYouSponsorsSection/ThankYouSponsorsSection.js
+++ src/components/Home/ThankYouSponsorsSection/ThankYouSponsorsSection.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
+export const SectionContainer = styled.span` 
+    height: 20em;
+`
+
+export const SectionHeadingContainer = styled.div`
+    text-align: center;
+    padding-bottom: 2em;
+    padding-left: 4em;
+    padding-right: 4em;
+`
+
+
+
 export default class ThankYouSponsorsSection extends React.Component {
     render() {
         return (
-            <div className="centred-container">
-                <h2>Thank you to our partners and sponsors</h2>
+            <SectionContainer>
+                <SectionHeadingContainer>
+                    <h2>Thank you to our partners and sponsors</h2>
+                </SectionHeadingContainer>
                 <div className="flex-grid">
                     <div className="flex-container">
                         <img src="" alt="Logo of Company" />
@@ -20,7 +35,7 @@ export default class ThankYouSponsorsSection extends React.Component {
                         <img src="" alt="Logo of Company" />
                     </div>
                 </div>
-            </div>
+            </SectionContainer>
         )
     }
 }
