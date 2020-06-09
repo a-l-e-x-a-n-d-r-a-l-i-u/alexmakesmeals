bfbf1e09ce199129c5ada5d78ceadbe052e5dee8
Elaine Gao <gao.elaine164@gmail.com>
2020-06-09 18:50:26 +1000

    Revert "remove duplicate section"
    This reverts commit 8b8e31fc45c1e78e3ca602ee484045fb1b2d9c0e.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,6 +12,7 @@ const KaelanBg = { backgroundImage: 'url(' + KaelanPortrait + ')'};
 const Home = () => {
     return (<>
         <section className="has-bg-img" style={KaelanBg}><HeroSection /></section>
+        <section className="has-bg-img" ><HeroSection /></section>
         <section className="plain-bg"><ImpactSection /></section>
         <section className="alternate-bg-2"><TestimonialSection/></section>
         <section className="plain-bg"><ThankYouSponsorsSection /></section>
