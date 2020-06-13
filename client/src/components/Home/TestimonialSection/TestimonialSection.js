29f4434446e6a71269f61fc9922e18650e24af31
Alie <hi@hi.com>
2020-06-13 23:54:53 +1000

    heroku demo

--- client/src/components/Home/TestimonialSection/TestimonialSection.js
+++ client/src/components/Home/TestimonialSection/TestimonialSection.js
@@ -5,7 +5,7 @@ export default class TestimonialSection extends React.Component {
         return (
             <div className="centred-container">
                 {/* <img id="impact-quote" alt="Kelsey Hibberd" /> */}
-                <blockquote><h3>“</h3>Knowing about this initiative, I was motivated push through and work my night shift tonight. I have volunteered to work nights, working 50-hour night-shift weeks to assist. And whilst I can head to Coles, my energy levels are running very low. A few meals to have as backup would help me an enormous amount for when I really have nothing left in the tank.”</blockquote>
+                <blockquote><span style={{ fontSize: "xx-large"}}>“</span>Knowing about this initiative, I was motivated push through and work my night shift tonight. I have volunteered to work nights, working 50-hour night-shift weeks to assist. And whilst I can head to Coles, my energy levels are running very low. A few meals to have as backup would help me an enormous amount for when I really have nothing left in the tank.<span style={{fontSize: "x-large"}}>”</span></blockquote>
                 <p>— Kelsey</p>
                 <p>Nurse at a major metro hospital</p>
             </div>
