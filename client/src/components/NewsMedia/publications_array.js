70b149ef2892740ba71232126170cdcb9440305f
Alie <hi@hi.com>
2020-06-14 12:21:57 +1000

    added news logos

--- client/src/components/NewsMedia/publications_array.js
+++ client/src/components/NewsMedia/publications_array.js
@@ -1,19 +1,57 @@
 import sunriseLogo from '../../images/publications/sunrise.png';
 import independentLogo from '../../images/publications/independent.png';
 import theAgeLogo from '../../images/publications/the-age.png';
+import mojoNewsLogo from '../../images/publications/mojo-news.jpg';
+import ABCLogo from '../../images/publications/abc.svg';
+import SBSLogo from '../../images/publications/sbs.svg';
+import whereToLogo from '../../images/publications/where-to.jpg';
+import redRoosterLogo from '../../images/publications/red-rooster.svg';
 
 const publications = [
-    { key: 1, title: 'The Uni Student Making Healthy Meals For Our Health Workers', publisher: 'Independent', logo: independentLogo, date: 'Apr 5 2020', url: 'https://10play.com.au/theproject/exclusives/2019/the-uni-student-making-healthy-meals-for-our-health-workers/tpv200405dlrwc' },
-    { key: 2, title: 'First a humble lasagne, then ‘loaves and fishes’ to feed healthcare heroes', publisher: 'The Age', logo: theAgeLogo, date: 'Apr 2 2020', url: 'https://www.theage.com.au/lifestyle/health-and-wellness/first-a-humble-lasagne-then-loaves-and-fishes-to-feed-healthcare-heroes-20200401-p54g38.html?fbclid=IwAR2gNObIgJq_DYiDG2WwM7K0IFivUlPU_CXfsk2I-5eMiyCK88Cmwx9NTSI' },
-    { key: 3, title: 'Coronavirus: Uni student making thousands of meals for exhausted health workers', publisher: 'Sunrise', logo: sunriseLogo, date: 'Apr 10 2020', url: 'https://7news.com.au/sunrise/on-the-show/coronavirus-melbourne-uni-student-making-thousands-of-meals-for-exhausted-health-workers-c-969049' },
-    { key: 4, title: 'Melbourne teenager Alex Dekker helping frontline health workers with meals', publisher: 'ABC Radio', date: 'Apr 5 2020', url: 'https://www.abc.net.au/radio/melbourne/programs/melbourneweekends/meet-the-teenager-feeding-melbournes-medics/12123188' },
-    { key: 5, title: 'Meet the 20-year-old making meals for our frontline health workers', publisher: 'ABC Radio', date: 'Apr 6 2020', url: 'https://www.abc.net.au/radionational/programs/breakfast/alex-makes-meals/12219076?fbclid=IwAR1sEQBsR-GhC4zrJRMqUDglyeolnFNauzIFm2S0qvr_BcynOsdFPRKAbDQ' },
-    { key: 6, title: 'From lasagne for his doctor sister to thousands of meals for health workers', publisher: 'SBS', date: 'Apr 13 2020', url: 'https://www.sbs.com.au/food/article/2020/04/13/lasagne-his-doctor-sister-thousands-meals-health-workers?fbclid=IwAR2CjI_4ChRYevL8Sigmrr97WcnK4xB9pwawJt8A80nBli3gyDVKmw4sxJU' },
-    { key: 7, title: 'Alex Makes Meals', publisher: 'Where To', date: 'Apr 10 2020', url: 'https://www.whereto.media/cityguide/melbourne/alex-makes-meals/?fbclid=IwAR1pnWlK-HAfUPqtITMcm5mQ5IQhFpZrJOQyoYThv3Xqi_QVOkS-KjUK_hc' },
-    { key: 8, title: 'If You’re In The Mood For Some Good News, Here Are 12 Local Stories For You', publisher: 'Where To', date: 'Apr 17 2020', url: 'https://www.buzzfeed.com/marielasummerhays01/feel-good-australian-stories-coronavirus?origin=web-hf' },
-    { key: 9, title: 'Red Rooster helps Alex Makes Meals deliver food to healthcare workers', publisher: 'Franchise Business', date: 'Apr 10 2020', url: 'https://www.franchisebusiness.com.au/red-rooster-alex-makes-meals-food-deliveries/' },
-    { key: 10, title: 'Monash student becomes healthcare workers’ saviour by rescuing food amid pandemic', publisher: 'Mojo News', date: 'Apr 10 2020', url: 'https://www.mojonews.com.au/monash-student-makes-meals-for-healthcare-professionals-working-during-covid-19' },
-    { key: 11, title: 'Student Provides Thousands of Homemade Meals to Healthcare Workers', publisher: 'Independent', logo: independentLogo, date: 'Apr 10 2020', url: 'https://www.independent.co.uk/life-style/australian-student-coronavirus-free-meals-healthcare-workers-a9458911.html?fbclid=IwAR1kSa_EkxMgp2Uo_WCQMGHUbKm1xMtSK2kazj7ZLcdg6DZOIuFXEjHjRpU' },
+    {
+        key: 1, title: 'The Uni Student Making Healthy Meals For Our Health Workers',
+        publisher: 'Independent', logo: independentLogo, date: 'Apr 5 2020', url: 'https://10play.com.au/theproject/exclusives/2019/the-uni-student-making-healthy-meals-for-our-health-workers/tpv200405dlrwc'
+    },
+    {
+        key: 2, title: 'First a humble lasagne, then ‘loaves and fishes’ to feed healthcare heroes',
+        publisher: 'The Age', logo: theAgeLogo, date: 'Apr 2 2020', url: 'https://www.theage.com.au/lifestyle/health-and-wellness/first-a-humble-lasagne-then-loaves-and-fishes-to-feed-healthcare-heroes-20200401-p54g38.html?fbclid=IwAR2gNObIgJq_DYiDG2WwM7K0IFivUlPU_CXfsk2I-5eMiyCK88Cmwx9NTSI'
+    },
+    {
+        key: 3, title: 'Coronavirus: Uni student making thousands of meals for exhausted health workers',
+        publisher: 'Sunrise', logo: sunriseLogo, date: 'Apr 10 2020', url: 'https://7news.com.au/sunrise/on-the-show/coronavirus-melbourne-uni-student-making-thousands-of-meals-for-exhausted-health-workers-c-969049'
+    },
+    {
+        key: 4, title: 'Melbourne teenager Alex Dekker helping frontline health workers with meals',
+        publisher: 'ABC Radio', logo: ABCLogo, date: 'Apr 5 2020', url: 'https://www.abc.net.au/radio/melbourne/programs/melbourneweekends/meet-the-teenager-feeding-melbournes-medics/12123188'
+    },
+    {
+        key: 5, title: 'Meet the 20-year-old making meals for our frontline health workers',
+        publisher: 'ABC Radio', logo: ABCLogo, date: 'Apr 6 2020', url: 'https://www.abc.net.au/radionational/programs/breakfast/alex-makes-meals/12219076?fbclid=IwAR1sEQBsR-GhC4zrJRMqUDglyeolnFNauzIFm2S0qvr_BcynOsdFPRKAbDQ'
+    },
+    {
+        key: 6, title: 'From lasagne for his doctor sister to thousands of meals for health workers',
+        publisher: 'SBS', logo: SBSLogo, date: 'Apr 13 2020', url: 'https://www.sbs.com.au/food/article/2020/04/13/lasagne-his-doctor-sister-thousands-meals-health-workers?fbclid=IwAR2CjI_4ChRYevL8Sigmrr97WcnK4xB9pwawJt8A80nBli3gyDVKmw4sxJU'
+    },
+    {
+        key: 7, title: 'Alex Makes Meals',
+        publisher: 'Where To', logo: whereToLogo, date: 'Apr 10 2020', url: 'https://www.whereto.media/cityguide/melbourne/alex-makes-meals/?fbclid=IwAR1pnWlK-HAfUPqtITMcm5mQ5IQhFpZrJOQyoYThv3Xqi_QVOkS-KjUK_hc'
+    },
+    {
+        key: 8, title: 'If You’re In The Mood For Some Good News, Here Are 12 Local Stories For You',
+        publisher: 'Where To', logo: whereToLogo, date: 'Apr 17 2020', url: 'https://www.buzzfeed.com/marielasummerhays01/feel-good-australian-stories-coronavirus?origin=web-hf'
+    },
+    {
+        key: 9, title: 'Red Rooster helps Alex Makes Meals deliver food to healthcare workers',
+        publisher: 'Franchise Business', logo: redRoosterLogo, date: 'Apr 10 2020', url: 'https://www.franchisebusiness.com.au/red-rooster-alex-makes-meals-food-deliveries/'
+    },
+    {
+        key: 10, title: 'Monash student becomes healthcare workers’ saviour by rescuing food amid pandemic',
+        publisher: 'Mojo News', logo: mojoNewsLogo, date: 'Apr 10 2020', url: 'https://www.mojonews.com.au/monash-student-makes-meals-for-healthcare-professionals-working-during-covid-19'
+    },
+    {
+        key: 11, title: 'Student Provides Thousands of Homemade Meals to Healthcare Workers',
+        publisher: 'Independent', logo: independentLogo, date: 'Apr 10 2020', url: 'https://www.independent.co.uk/life-style/australian-student-coronavirus-free-meals-healthcare-workers-a9458911.html?fbclid=IwAR1kSa_EkxMgp2Uo_WCQMGHUbKm1xMtSK2kazj7ZLcdg6DZOIuFXEjHjRpU'
+    },
 ];
 
 export default publications
