21bb7a84b6bc8e42a484b6201323c95efc02e4a2
Alie <hi@hi.com>
2020-06-21 13:32:16 +1000

    Updated copy to align to AMM strategic pivot

--- client/src/components/Home/HeroSection/HeroSection.js
+++ client/src/components/Home/HeroSection/HeroSection.js
@@ -5,8 +5,8 @@ export default class HeroSection extends React.Component {
         return (
             <div className="large">
                 <div className="hero-container">
-                    <h1>Help to feed frontline workers – those who take care of you and your loved ones.</h1>
-                    <h3>Support all medical and ancillary professionals at the frontline of the COVID-19 pandemic.</h3>
+                    <h1>Help to feed those that face food insecurity, who need our support.</h1>
+                    <h3>5 million Australian have struggled to get meals on the table everyday.</h3>
                     <div className="button"><a href="/donate"><h4>Make A Donation</h4></a></div>
                     <div className="button button-secondary"><a href="/partners"><h4>Corporates</h4></a></div>
                 </div>
